c17ffd0e1386073580b678e184ed1d72e2c1cf92
Oliver Dippel <odippel@ypsilon.net>
2023-03-21 16:52:06 +0100

    adding 2d grid

--- viaconstructor/draw2d.py
+++ viaconstructor/draw2d.py
@@ -76,6 +76,15 @@ def draw_line_2d(p_from: Sequence[float], p_to: Sequence[float]) -> None:
     )
 
 
+def draw_circle_2d(p_center: Sequence[float], p_size: float) -> None:
+    painter["ctx"].drawEllipse(  # type: ignore
+        (painter["offset_x"] + p_center[0] - p_size / 2.0) * painter["scale"],  # type: ignore
+        (painter["offset_y"] + p_center[1] - p_size / 2.0) * -painter["scale"],  # type: ignore
+        p_size * painter["scale"],  # type: ignore
+        p_size * -painter["scale"],  # type: ignore
+    )
+
+
 def draw_mill_line(
     p_from: Sequence[float],
     p_to: Sequence[float],
@@ -85,6 +94,12 @@ def draw_mill_line(
     project: dict,  # pylint: disable=W0613
 ) -> None:
     """draws an milling line including direction and width"""
+
+    if p_from[2] < 0.0 and p_to[2] < 0.0:
+        painter["ctx"].setPen(QPen(QtCore.Qt.green, 1, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
+    else:
+        painter["ctx"].setPen(QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
+
     draw_line_2d(p_from, p_to)
 
 
@@ -112,7 +127,7 @@ def draw_object_edges(
             depths.append(odepth)
     depths.sort()
 
-    painter["ctx"].setPen(QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
+    painter["ctx"].setPen(QPen(QtCore.Qt.white, 2, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
     for _obj_idx, obj in project["objects"].items():
         if obj.get("layer", "").startswith("BREAKS:") or obj.get(
             "layer", ""
@@ -145,9 +160,7 @@ def draw_line(p_1: dict, p_2: dict, options: str, project: dict) -> None:
         )
     line_width = project["setup"]["tool"]["diameter"]
     mode = project["setup"]["view"]["path"]
-
     project["simulation_data"].append((p_from, p_to, line_width, mode, options))
-
     draw_mill_line(p_from, p_to, line_width, mode, options, project)
 
 
@@ -170,23 +183,64 @@ def draw_machinecode_path(project: dict) -> bool:
     return True
 
 
+def draw_grid(project: dict) -> None:
+    """draws the grid"""
+    min_max = project["minMax"]
+    size = project["setup"]["view"]["grid_size"]
+    start_x = int(min_max[0] / size) * size - size
+    end_x = int(min_max[2] / size) * size + size
+    start_y = int(min_max[1] / size) * size - size
+    end_y = int(min_max[3] / size) * size + size
+    z_offset = -project["setup"]["workpiece"]["offset_z"]
+    mill_depth = project["setup"]["mill"]["depth"]
+    unit = project["setup"]["machine"]["unit"]
+    unitscale = 1.0
+    if unit == "inch":
+        unitscale = 25.4
+        z_offset *= unitscale
+        mill_depth *= unitscale
+
+    painter["ctx"].setPen(QPen(QtCore.Qt.gray, 1, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
+    if project["setup"]["view"]["grid_show"]:
+        # Grid-X
+        for p_x in range(start_x, end_x + size, size):
+            draw_line_2d((p_x, start_y), (p_x, end_y))
+        # Grid-Y
+        for p_y in range(start_y, end_y + size, size):
+            draw_line_2d((start_x, p_y), (end_x, p_y))
+
+    # Zero-Point
+    painter["ctx"].setPen(QPen(QtCore.Qt.yellow, 1, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
+    draw_line_2d((0.0, start_y), (0.0, end_y))
+    draw_line_2d((start_x, 0.0), (end_x, 0.0))
+
+    painter["ctx"].setPen(QPen(QtCore.Qt.yellow, 3, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
+    draw_circle_2d((0, 0), 3)
+
+
 def draw_all(project: dict) -> None:
 
     min_max = project["minMax"]
     if not min_max:
         return
 
+    s_w = 1600
+    s_h = 1600
+
     size_x = max(min_max[2] - min_max[0], 0.1)
     size_y = max(min_max[3] - min_max[1], 0.1)
-    painter["scale"] = min(1200.0 / size_x, 1200.0 / size_y) / 1.4
-    painter["offset_x"] = -min_max[0] - size_x / 2 + (600 / painter["scale"])  # type: ignore
-    painter["offset_y"] = -size_y / 2 - min_max[1] - (600 / painter["scale"])  # type: ignore
+    painter["scale"] = min(s_w / size_x, s_h / size_y) / 1.4
+    painter["offset_x"] = -min_max[0] - size_x / 2 + (s_w / 2 / painter["scale"])  # type: ignore
+    painter["offset_y"] = -size_y / 2 - min_max[1] - (s_h / 2 / painter["scale"])  # type: ignore
 
-    canvas = QPixmap(1200, 1200)
+    canvas = QPixmap(s_w, s_h)
     project["glwidget"].setPixmap(canvas)
     project["glwidget"].adjustSize()
     project["glwidget"].pixmap().fill(QtCore.Qt.black)  # type: ignore  # pylint: disable=I1101
     painter["ctx"]: QPainter = QPainter(project["glwidget"].pixmap())  # type: ignore
+
+    draw_grid(project)
+
     painter["ctx"].setPen(QPen(QtCore.Qt.green, 1, QtCore.Qt.SolidLine))  # type: ignore  # pylint: disable=I1101
 
     if project["glwidget"]:
