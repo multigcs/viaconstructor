9bf8c4bcb84123eb5d589afd7b24ba7f5f94e096
Oliver Dippel <odippel@ypsilon.net>
2024-04-04 15:32:19 +0200

    fixing test

--- tests/test_machine_cmds.py
+++ tests/test_machine_cmds.py
@@ -172,6 +172,7 @@ class fakeOffset:
                         "mode": "mill",
                         "unit": "mm",
                         "comments": True,
+                        "arcs": "ij",
                         "g54": False,
                         "supports_toolchange": True,
                         "toolchange_pre": "",
