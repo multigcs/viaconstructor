57e23b397131e3a8e14796280d1386ae6ebe06f7
Oliver Dippel <odippel@ypsilon.net>
2024-04-23 15:44:51 +0200

    box: adding ToolTips / top plate

--- viaconstructor/tools/box.py
+++ viaconstructor/tools/box.py
@@ -1,3 +1,4 @@
+import os
 import math
 import tempfile
 
@@ -9,9 +10,27 @@ from PyQt5.QtWidgets import (  # pylint: disable=E0611
     QLabel,
     QPushButton,
     QSpinBox,
+    QCheckBox,
     QVBoxLayout,
     QWidget,
 )
+
+# i18n
+def no_translation(text):
+    return text
+
+
+_ = no_translation
+lang = os.environ.get("LANGUAGE")
+if lang:
+    localedir = os.path.join(Path(__file__).resolve().parent, "locales")
+    try:
+        lang_translations = gettext.translation("base", localedir=localedir, languages=[lang])
+
+        lang_translations.install()
+        _ = lang_translations.gettext
+    except FileNotFoundError:
+        sys.stderr.write(f"WARNING: localedir not found {localedir}\n")
 
 
 class BoxTool(QWidget):
@@ -19,12 +38,13 @@ class BoxTool(QWidget):
 
     def __init__(self, app):
         super().__init__()
-        self.setWindowTitle("box-Tool")
+        self.setWindowTitle("Box-Tool")
         self.app = app
         layout = QVBoxLayout()
         self.setLayout(layout)
 
         label = QLabel("Width")
+        label.setToolTip(_("width of the box"))
         layout.addWidget(label)
         self.box_width = QDoubleSpinBox()
         self.box_width.setDecimals(2)
@@ -32,9 +52,11 @@ class BoxTool(QWidget):
         self.box_width.setMinimum(-100000)
         self.box_width.setMaximum(100000)
         self.box_width.setValue(200.0)
+        self.box_width.setToolTip(_("width of the box"))
         layout.addWidget(self.box_width)
 
         label = QLabel("Height")
+        label.setToolTip(_("height of the box"))
         layout.addWidget(label)
         self.box_height = QDoubleSpinBox()
         self.box_height.setDecimals(2)
@@ -42,9 +64,11 @@ class BoxTool(QWidget):
         self.box_height.setMinimum(-100000)
         self.box_height.setMaximum(100000)
         self.box_height.setValue(50.0)
+        self.box_height.setToolTip(_("height of the box"))
         layout.addWidget(self.box_height)
 
         label = QLabel("Depth")
+        label.setToolTip(_("depth of the box"))
         layout.addWidget(label)
         self.box_depth = QDoubleSpinBox()
         self.box_depth.setDecimals(2)
@@ -52,9 +76,11 @@ class BoxTool(QWidget):
         self.box_depth.setMinimum(-100000)
         self.box_depth.setMaximum(100000)
         self.box_depth.setValue(100.0)
+        self.box_depth.setToolTip(_("depth of the box"))
         layout.addWidget(self.box_depth)
 
         label = QLabel("Tooth-Len")
+        label.setToolTip(_("len of the tooth"))
         layout.addWidget(label)
         self.box_tooth = QDoubleSpinBox()
         self.box_tooth.setDecimals(2)
@@ -62,9 +88,11 @@ class BoxTool(QWidget):
         self.box_tooth.setMinimum(-100000)
         self.box_tooth.setMaximum(100000)
         self.box_tooth.setValue(10.0)
+        self.box_tooth.setToolTip(_("len of the tooth"))
         layout.addWidget(self.box_tooth)
 
         label = QLabel("Thickness")
+        label.setToolTip(_("thickness of the material"))
         layout.addWidget(label)
         self.box_thickness = QDoubleSpinBox()
         self.box_thickness.setDecimals(2)
@@ -72,7 +100,16 @@ class BoxTool(QWidget):
         self.box_thickness.setMinimum(-100000)
         self.box_thickness.setMaximum(100000)
         self.box_thickness.setValue(3.0)
+        self.box_thickness.setToolTip(_("thickness of the material"))
         layout.addWidget(self.box_thickness)
+
+        label = QLabel("Top")
+        label.setToolTip(_("with top plate"))
+        layout.addWidget(label)
+        self.box_top = QCheckBox()
+        self.box_top.setChecked(True)
+        self.box_top.setToolTip(_("with top plate"))
+        layout.addWidget(self.box_top)
 
         self.qimage_label = QLabel()
         layout.addWidget(self.qimage_label)
@@ -85,6 +122,7 @@ class BoxTool(QWidget):
             depth = self.box_depth.value()
             tooth_len = self.box_tooth.value()
             thickness = self.box_thickness.value()
+            top = self.box_top.isChecked()
             rad = 0
 
             print(f"size: {width} x {height} x {depth}")
@@ -133,7 +171,7 @@ class BoxTool(QWidget):
                     points_fb.append((width - thickness, start_height + n * tooth_len * 2 + tooth_len))
                 points_fb.append((width, start_height + n * tooth_len * 2 + tooth_len))
             points_fb.append((width, height))
-            if True:  # args.top
+            if top:
                 for n in range(0, int(num_tooth_width)):
                     points_fb.append((width - (start_width + n * tooth_len * 2), height))
                     if rad:
@@ -317,11 +355,11 @@ class BoxTool(QWidget):
                     points_side.append((depth, start_height + n * tooth_len * 2))
                     points_side.append((depth, start_height + n * tooth_len * 2 + tooth_len))
                     points_side.append((depth - thickness, start_height + n * tooth_len * 2 + tooth_len))
-            if True:  # args.top
+            if top:
                 points_side.append((depth - thickness, height - thickness))
             else:
                 points_side.append((depth - thickness, height))
-            if True:  # args.top
+            if top:
                 for n in range(0, int(num_tooth_depth)):
                     if rad:
                         points_side.append(
@@ -411,7 +449,7 @@ class BoxTool(QWidget):
                 "left": (points_side, (width + 20) * 2, 0),
                 "right": (points_side, (width + 20) * 2, height + 20),
             }
-            if True:  # args.top
+            if top:
                 layers["bottom"] = (points_top, width + 20, height + 20)
                 layers["top"] = (points_top, 0, height + 20)
             else:
