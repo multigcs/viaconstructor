f284097d482600b5b77b34168f78bb7f88e5f7ea
Oliver Dippel <odippel@ypsilon.net>
2024-07-18 11:51:30 +0200

    cdrreader: prepare for more path commands

--- viaconstructor/input_plugins/cdrread.py
+++ viaconstructor/input_plugins/cdrread.py
@@ -42,44 +42,73 @@ class DrawReader(DrawReaderBase):
                 for part in res:
                     parts = part.split(",")
                     atype = parts[0]
-                    if atype in {"M", "L", "C"}:
-                        cords = {}
-                        for cord in parts[1:]:
-                            splitted = cord.split(":")
-                            name = splitted[1]
-                            value = splitted[2].strip()
-                            if value.endswith("in"):
-                                # value = float(value[:-2]) * 25.4
-                                value = float(value[:-2]) * 72
-                            cords[name] = value
-                        if atype == "M":
-                            last_x = cords["x"]
-                            last_y = cords["y"]
-                        elif atype == "L":
-                            self._add_line((last_x, last_y), (cords["x"], cords["y"]))
-                            last_x = cords["x"]
-                            last_y = cords["y"]
-                        elif atype == "C":
-                            p0 = np.array([last_x, last_y])
-                            p1 = np.array([cords["x1"], cords["y1"]])
-                            p2 = np.array([cords["x2"], cords["y2"]])
-                            p3 = np.array([cords["x"], cords["y"]])
-                            t_values = np.linspace(0, 1, args.cdrread_curveres)
-                            x_values = []
-                            y_values = []
-                            for t in t_values:
-                                x, y = cubic_bezier(t, p0, p1, p2, p3)
-                                x_values.append(x)
-                                y_values.append(y)
-                            for pn in range(args.cdrread_curveres):
-                                xpos = x_values[pn]
-                                ypos = y_values[pn]
-                                self._add_line((last_x, last_y), (xpos, ypos))
-                                last_x = xpos
-                                last_y = ypos
-                            self._add_line((last_x, last_y), (cords["x"], cords["y"]))
-                            last_x = cords["x"]
-                            last_y = cords["y"]
+                    cords = {}
+                    for cord in parts[1:]:
+                        splitted = cord.split(":")
+                        name = splitted[1]
+                        value = splitted[2].strip()
+                        if value.endswith("in"):
+                            # value = float(value[:-2]) * 25.4
+                            value = float(value[:-2]) * 72
+                        cords[name] = value
+                    if atype == "M":
+                        # moveto (move from one point to another point)
+                        last_x = cords["x"]
+                        last_y = cords["y"]
+                    elif atype == "Z":
+                        # closepath (close the path)
+                        pass
+                    elif atype == "L":
+                        # lineto (create a line)
+                        self._add_line((last_x, last_y), (cords["x"], cords["y"]))
+                        last_x = cords["x"]
+                        last_y = cords["y"]
+                    elif atype in {"Q", "T"}:
+                        # quadratic Bézier curve (create a quadratic Bézier curve)
+                        # smooth quadratic Bézier curveto (create a smooth quadratic Bézier curve)
+                        self._add_line((last_x, last_y), (cords["x"], cords["y"]))
+                        last_x = cords["x"]
+                        last_y = cords["y"]
+                    elif atype == "A":
+                        # elliptical Arc (create a elliptical arc)
+                        self._add_line((last_x, last_y), (cords["x"], cords["y"]))
+                        last_x = cords["x"]
+                        last_y = cords["y"]
+                    elif atype == "H":
+                        # horizontal lineto (create a horizontal line)
+                        self._add_line((last_x, last_y), (cords["x"], last_y))
+                        last_x = cords["x"]
+                    elif atype == "V":
+                        # vertical lineto (create a vertical line)
+                        self._add_line((last_x, last_y), (last_x, cords["y"]))
+                        last_y = cords["y"]
+                    elif atype in {"C", "S"}:
+                        # curveto (create a curve)
+                        # smooth curveto (create a smooth curve)
+                        p0 = np.array([last_x, last_y])
+                        p1 = np.array([cords["x1"], cords["y1"]])
+                        p2 = np.array([cords["x2"], cords["y2"]])
+                        p3 = np.array([cords["x"], cords["y"]])
+                        t_values = np.linspace(0, 1, args.cdrread_curveres)
+                        x_values = []
+                        y_values = []
+                        for t in t_values:
+                            x, y = cubic_bezier(t, p0, p1, p2, p3)
+                            x_values.append(x)
+                            y_values.append(y)
+                        for pn in range(args.cdrread_curveres):
+                            xpos = x_values[pn]
+                            ypos = y_values[pn]
+                            self._add_line((last_x, last_y), (xpos, ypos))
+                            last_x = xpos
+                            last_y = ypos
+                        self._add_line((last_x, last_y), (cords["x"], cords["y"]))
+                        last_x = cords["x"]
+                        last_y = cords["y"]
+                    elif "x" in cords and "y" in cords:
+                        self._add_line((last_x, last_y), (cords["x"], cords["y"]))
+                        last_x = cords["x"]
+                        last_y = cords["y"]
 
     @staticmethod
     def arg_parser(parser) -> None:
